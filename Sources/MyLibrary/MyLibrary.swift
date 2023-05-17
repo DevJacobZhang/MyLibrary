53bff4fa14011b93a4faf05b25a7b891d4522cbf
DevJacobZhang <r25244849@gmail.com>
2023-05-17 15:04:30 +0800

    +view +string

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -3,7 +3,7 @@ import UIKit
 
 extension UIView {
     ///設置圓角
-    public func addRoundedCorners(corners: UIRectCorner, radius: CGFloat) {
+    public func CRAddRoundedCorners(corners: UIRectCorner, radius: CGFloat) {
         let path = UIBezierPath(roundedRect: self.bounds,
                                 byRoundingCorners: corners,
                                 cornerRadii: CGSize(width: radius, height: radius))
@@ -13,39 +13,24 @@ extension UIView {
     }
 }
 
-extension UILabel {
-    public func testWord() {
-        
+extension String {
+    ///將第一個字元改成大寫，其餘後面全部小寫
+    func CRCapitalizeFirstLetter() -> String {
+        return self.prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+    
+    ///回傳這筆字串在UILabel裡需要多少高
+    func CRCalculateLabelHeight(font: UIFont, width: CGFloat) -> CGFloat {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.font = font
+        label.text = self
+        label.sizeToFit()
+        return label.frame.height
     }
 }
 
 public struct MyLibrary {
-    
-    ///將第一個字元改成大寫，其餘後面全部小寫
-    static public func capitalizeFirstLetter(string: String) -> String {
-        return string.prefix(1).uppercased() + string.lowercased().dropFirst()
-    }
-    
-    ///設置圓角
-//    static public func addRoundedCorners(view: UIView, corners: UIRectCorner, radius: CGFloat) {
-//        let path = UIBezierPath(roundedRect: view.bounds,
-//                                byRoundingCorners: corners,
-//                                cornerRadii: CGSize(width: radius, height: radius))
-//        let maskLayer = CAShapeLayer()
-//        maskLayer.path = path.cgPath
-//        view.layer.mask = maskLayer
-//    }
-    
-    ///回傳這筆字串在UILabel裡需要多少高
-    static public func calculateLabelHeight(text: String, font: UIFont, width: CGFloat) -> CGFloat {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude))
-        label.numberOfLines = 0
-        label.font = font
-        label.text = text
-        label.sizeToFit()
-        return label.frame.height
-    }
-    
     ///取得UIWindow
     static public func getKeyWindow() -> UIWindow? {
         
---
+++ Sources/MyLibrary/View+.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 張家豪 on 2023/5/17.
+//
+
+import Foundation
